6f6be914666918d01883f48964130e9438c98768
ViniTvll <vinitavella18@gmail.com>
2024-09-25 19:44:20 -0500

    ok

---
+++ src/test/java/com/example/lab5_gtics_20182693/Lab5Gtics20182693ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab5_gtics_20182693;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab5Gtics20182693ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
