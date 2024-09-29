9a331ebc6a481e33d24a1944575b1e95b8f55253
ViniTvll <vinitavella18@gmail.com>
2024-09-29 15:52:52 -0500

    No me sale el filtro pipipi

--- src/main/java/com/example/lab5_gtics_20182693/controller/ProfesionalesController.java
+++ src/main/java/com/example/lab5_gtics_20182693/controller/ProfesionalesController.java
@@ -1,11 +1,16 @@
 package com.example.lab5_gtics_20182693.controller;
 import com.example.lab5_gtics_20182693.entity.Profesionales;
+import com.example.lab5_gtics_20182693.repository.AreaRepository;
 import com.example.lab5_gtics_20182693.repository.ProfesionalesRepository;
+import com.example.lab5_gtics_20182693.repository.SedeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,18 +18,47 @@ import java.util.List;
 public class ProfesionalesController {
 
     final ProfesionalesRepository profesionalesRepository;
+    final AreaRepository areaRepository;
+    final SedeRepository sedeRepository;
 
-    public ProfesionalesController(ProfesionalesRepository profesionalesRepository) {this.profesionalesRepository = profesionalesRepository;}
+    public ProfesionalesController(
+            ProfesionalesRepository profesionalesRepository, AreaRepository areaRepository, SedeRepository sedeRepository) {
+             this.profesionalesRepository = profesionalesRepository;
+             this.areaRepository = areaRepository;
+             this.sedeRepository = sedeRepository;
+    }
 
     List<Profesionales> profesionales = new ArrayList<>();
 
     @GetMapping("/profesionales")
-    public String listarProfesionales(
-
-            Model model) {
+    public String listarProfesionales(Model model) {
         model.addAttribute("profesionales", profesionalesRepository.findAll());
+        model.addAttribute("listaAreas", areaRepository.findAll());
+        model.addAttribute("listaSedes", sedeRepository.findAll());
+
         return "profesionales";
     }
+
+    @GetMapping("/profesionales/filtrar")
+    public String filtrarProfesionales(
+            @RequestParam(value = "nombreProfesional", required = false) String nombreProfesional,
+            @RequestParam(value = "idArea", required = false) Integer idArea,
+            @RequestParam(value = "idSede", required = false) Integer idSede,
+            @RequestParam(value = "fecha", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fecha,
+            Model model) {
+
+        List<Profesionales> listaProfesionales = profesionalesRepository.buscarPorNombreAreaSede(nombreProfesional, idArea, idSede, fecha);
+
+        model.addAttribute("listaProfesionales", listaProfesionales);
+
+        model.addAttribute("listaAreas", areaRepository.findAll());
+        model.addAttribute("listaSedes", sedeRepository.findAll());
+
+        // Retornar la vista correspondiente
+        return "profesionales/gestion_profesionales";
+    }
+
+
 }
 
 
--- src/main/java/com/example/lab5_gtics_20182693/repository/ProfesionalesRepository.java
+++ src/main/java/com/example/lab5_gtics_20182693/repository/ProfesionalesRepository.java
@@ -6,10 +6,24 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface ProfesionalesRepository extends JpaRepository<Profesionales, Integer> {
 
+
+    @Query("SELECT p FROM Profesionales p " +
+            "JOIN p.area a " +
+            "JOIN p.sede s " +
+            "JOIN p.fechaDisponibilidad f " +
+            "WHERE (:nombreProfesional IS NULL OR p.nombre LIKE %:nombreProfesional%) " +
+            "AND (:idArea IS NULL OR a.idAreas = :idArea) " +
+            "AND (:idSede IS NULL OR s.idSedes = :idSede) " +
+            "AND (:fechaDisponibilidad IS NULL OR f.fechaDisponibilidad = :fechaDisponibilidad)")
+    List<Profesionales> buscarPorNombreAreaSede(@Param("nombre") String nombre,
+                                           @Param("idArea") Integer idArea,
+                                           @Param("idSede") Integer idSede,
+                                           @Param("fechaDisponibilidad") LocalDate fechaDisponibilidad);
 
 }
